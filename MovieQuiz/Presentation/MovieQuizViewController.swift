18b8214a4c9ff436c0cc62c1f6c225af84b7696c
i.kruglova <i.kruglova@make.st>
2025-02-25 17:18:14 +0700

    upd script to apply fonts in function

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -4,10 +4,13 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private weak var textLabel: UILabel!
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var counterLabel: UILabel!
-    
+    @IBOutlet private weak var yesButton: UIButton!
+    @IBOutlet private weak var noButton: UIButton!
+    @IBOutlet private weak var titleTextLabel: UILabel!
+
     private var currentQuestionIndex = 0
     private var correctAnswers = 0
-    
+
     private let questions: [QuizQuestion] = [
         QuizQuestion(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
         QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
@@ -20,38 +23,47 @@ final class MovieQuizViewController: UIViewController {
         QuizQuestion(image: "Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
         QuizQuestion(image: "Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
     ]
-    
+
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         showCurrentQuestion()
+        setFonts()
     }
-    
+
     @IBAction private func yesButtonClicked(_ sender: Any) {
         showAnswerResult(isCorrect: questions[currentQuestionIndex].correctAnswer == true)
     }
-    
+
     @IBAction private func noButtonClicked(_ sender: Any) {
         showAnswerResult(isCorrect: questions[currentQuestionIndex].correctAnswer == false)
     }
-    
+
+    private func setFonts() {
+        textLabel.font = UIFont(name: "YSDisplay-Bold", size: 23)
+        counterLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        titleTextLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        yesButton.titleLabel?.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        noButton.titleLabel?.font = UIFont(name: "YSDisplay-Medium", size: 20)
+    }
+
     private func showCurrentQuestion() {
         let question = questions[currentQuestionIndex]
         let viewModel = convert(model: question)
         show(quiz: viewModel)
     }
-    
+
     private func showAnswerResult(isCorrect: Bool) {
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
-        
+
         if isCorrect { self.correctAnswers += 1 }
-        
+
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.showNextQuestionOrResults()
         }
     }
-    
+
     private func showNextQuestionOrResults() {
         if currentQuestionIndex == questions.count - 1 {
             let viewModel = QuizResultsViewModel(
@@ -60,13 +72,13 @@ final class MovieQuizViewController: UIViewController {
                 buttonText: "Сыграть ещё раз"
             )
             show(quiz: viewModel)
-            
+
         } else {
             currentQuestionIndex += 1
             showCurrentQuestion()
         }
     }
-    
+
     private func convert(model: QuizQuestion) -> QuizStepModel {
         return QuizStepModel(
             image: UIImage(named: model.image) ?? UIImage(),
@@ -74,47 +86,47 @@ final class MovieQuizViewController: UIViewController {
             questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)"
         )
     }
-    
+
     private func show(quiz step: QuizStepModel) {
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
         imageView.image = step.image
         imageView.layer.borderWidth = 0
     }
-    
+
     private func show(quiz result: QuizResultsViewModel) {
         let alert = UIAlertController(
             title: result.title,
             message: result.text,
             preferredStyle: .alert
         )
-        
+
         let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
             self.restartQuiz()
         }
-        
+
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
     }
-    
+
     private func restartQuiz() {
         self.currentQuestionIndex = 0
         self.correctAnswers = 0
         showCurrentQuestion()
     }
-    
+
     struct QuizQuestion {
         let image: String
         let text: String
         let correctAnswer: Bool
     }
-    
+
     struct QuizStepModel {
         let image: UIImage
         let question: String
         let questionNumber: String
     }
-    
+
     struct QuizResultsViewModel {
         let title: String
         let text: String
